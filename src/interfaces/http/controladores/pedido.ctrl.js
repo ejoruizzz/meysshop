d673ae1140a584df2fa435d825a7c3307e08f84a
ejoruizzz <ejoruiz@hotmail.com>
2025-09-14 18:23:24 -0600

    refactor: renombra variables de pedido

--- src/interfaces/http/controladores/pedido.ctrl.js
+++ src/interfaces/http/controladores/pedido.ctrl.js
@@ -5,13 +5,9 @@ const { pedidoSchema } = require('../validadores/pedido.val');
 
 async function realizarPedido(req, res, next) {
   try {
-
-    const pedido = await crearPedido(req.body);
-
     const datos = pedidoSchema.parse(req.body);
-    const pedido = crearPedido(datos);
-
-    res.status(201).json(pedido);
+    const nuevoPedido = await crearPedido(datos);
+    res.status(201).json(nuevoPedido);
   } catch (err) {
     next(err);
   }
@@ -29,11 +25,11 @@ async function obtenerHistorial(req, res, next) {
 
 async function cancelar(req, res, next) {
   try {
-    const pedido = await cancelarPedido(req.params.id);
-    if (!pedido) {
+    const pedidoCancelado = await cancelarPedido(req.params.id);
+    if (!pedidoCancelado) {
       return res.status(404).json({ error: 'Pedido no encontrado' });
     }
-    res.json(pedido);
+    res.json(pedidoCancelado);
   } catch (err) {
     next(err);
   }
